3e9a7f0cfbe1d60d08cfb2eab08a87c7f28985c2
jstuhli <josip.stuhli@gmail.com>
2012-02-28 21:12:31 +0100

    Modificirana xica tako da radi sa novim sustavom
    Signed-off-by: jstuhli <josip.stuhli@gmail.com>

---
+++ php/aspparser.php
@@ -0,0 +1,43 @@
+<?php
+Class ASPParser {
+    private $cookiejar;
+    private $data = array();
+    const METHOD_POST = 0;
+    const METHOD_GET = 1;
+
+    public function __construct() {
+        $this->cookiejar = tempnam('/tmp', 'xica');
+    }
+
+    public function getData($method, $url, $data=array()) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookiejar);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookiejar);
+
+        if($method == self::METHOD_POST) {
+            $data = array_merge($this->data, $data);
+            $data_string = '';
+            foreach ($data as $k=>$v)
+                $data_string .= $k . '=' . urlencode($v) . '&';
+            $data_string = substr($data_string, 0, -1);
+
+            curl_setopt($ch, CURLOPT_POST, count($data));
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+
+        }
+        
+        $this->parse_asp($response = curl_exec($ch));
+        curl_close($ch);
+        return $response;
+    }
+
+    private function parse_asp($parse_asp){
+		$this->data = array();
+		foreach(array("__VIEWSTATE","__EVENTVALIDATION") as $name)
+			if(preg_match('/id="'.$name.'" value="([^"]+)/', $parse_asp, $match))
+				$this->data[$name] = $match[1];
+	}
+}
--- php/xica.php
+++ php/xica.php
@@ -1,107 +1,154 @@
 <?php
-	include("inc.http.php");
-	$http = new http();
-	$ret=array();
-	$prava = array("0"=>0, "1"=> 49.2, "2" => 98.4, "2.5" => 123.0, "3" => 147.6);
-	
-	$jmbag=$_REQUEST["jmbag"]; // obavezno
-	$jmbg=$_REQUEST["jmbg"];   // obavezno
-	$racun=$_REQUEST["racun"]; // id racuna iz liste, pocevsi od 1
-	
-	$data="__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwULLTEwOTc4NDMyMjRkZEh%2FmRix953PbZj%2BTtS0h1oQWFd5&__EVENTVALIDATION=%2FwEWBALQip6yBAKZmN7rBAKcmN7rBALJjIyiAjzRaQ99%2F1TXylvhlDnEX%2FmFDeRw&ctl00%24rightframe%24Textbox2=".$jmbag."&ctl00%24rightframe%24Textbox1=".$jmbg."&ctl00%24rightframe%24Button1=Prijava";
-	
-	$cache="";
-	$cookie="";
-	$asp_data="";
-	if(file_exists("cache/$jmbag.txt") && file_exists("cache/$jmbag.cookie.txt") && file_exists("cache/$jmbag.aspdata.txt") && filemtime("cache/$jmbag.txt")+300>time()){
-		@$cache=file_get_contents("cache/$jmbag.txt");
-		@$cookie=file_get_contents("cache/$jmbag.cookie.txt");
-		@$asp_data=file_get_contents("cache/$jmbag.aspdata.txt");
-	}
-	if($cache==""){
-		$html=$http->PostHTML("www.cap.srce.hr","/Prijava.aspx",$data);
-		if(!ereg('<span id="ctl00_rightframe_Label1"><b>([^<]+)</b></span>',$html,$match))
-			die("FAIL: Krivi login podaci!");
-		$ret["korisnik"]=$match[1];
-		
-		if(!ereg('<span id="ctl00_rightframe_Label4">([^<]*)</span>',$html,$match))
-			die("FAIL: Greska prilikom ocitavanja racuna!");
-		$ret["stanje"]=(float)str_replace(",", ".",$match[1]);
-		
-		if(!ereg('<span id="ctl00_rightframe_Label3">([0-9.]+)',$html,$match))
-			die("FAIL: Greska prilikom ucitavanja razine prava");
-		$ret["razina"]=$match[1];
-		
-		if(ereg('<img id="ctl00_rightframe_Image1" src="TempPic.([0-9]*).jpg"',$html,$match)) {
-			$url="/TempPic\\".$match[1].".jpg";
-			$ret["slika"]=$url;
-		}
-		
-		
-		$html=$http->PostHTML("www.cap.srce.hr","/Student.aspx","__EVENTTARGET=ctl00%24rightframe%24LinkButton2".$http->parse_asp($html));
-		
-		$spent_total = 0.0;
-		if(ereg("<span id=\"ctl00_rightframe_Label3\">([0-9,]+)</span>",$html,$match)) {
-			$spent_total = str_replace(",", ".",$match[1]);
-		}
-		$ret["racuni"]=array();
-		$spent = 0.0;
-		
-		
+include('aspparser.php');
+
+Class Xica {
+    public $korisnik = '';
+    public $stanje = 0;
+    public $razinaPrava = 0;
+    public $slikaURL = '';
+    
+    public $racuni = array();
+    public $danasPotroseno = 0.0;
+    public $ukupnoPotroseno = 0.0;
+    
+    private $prava = array('0'=>0, '1'=> 49.2, '2' => 98.4, '2.5' => 123.0, '3' => 147.6);
+    
+    private $aspParser;
+    public function __construct() {
+        $this->aspParser = new ASPParser();
+    }
+    public function login($user, $pass, &$errorMsg = '') {
+        $html = $this->aspParser->getData(ASPParser::METHOD_GET, 'http://www.cap.srce.hr/login.ashx');
+        if(preg_match('/name="AuthState" value="([^"]+)/', $html, $match)) {
+            $data['AuthState'] = str_replace('&amp;', '&', $match[1]);
+            $data['username'] = $user;
+            $data['password'] = $pass;
+            $data['Submit'] = 'Prijavi se';
+            $html = $this->aspParser->getData(ASPParser::METHOD_POST, 'https://login.aaiedu.hr/sso/module.php/core/loginuserpass.php?', $data);
+            if(preg_match('/name="SAMLResponse" value="([^"]+)/', $html, $match)) {
+                $html = $this->aspParser->getData(ASPParser::METHOD_POST, 'http://www.cap.srce.hr/login.ashx', array('SAMLResponse' => $match[1]));
+                if(preg_match('%<span id="rightframe_ImePrezimeLabel"><b><font face="Verdana" size="3">([^<]+?)</font></b></span>%', $html, $match))
+                    $this->korisnik = $match[1];
+                else {
+                    $errorMsg = 'Greska prilikom ocitavanja imena';
+                    return false;
+                }
+                if(preg_match('%<span id="rightframe_PreostalaSubvencijaLabel"><font face="Verdana" size="2">([^<]+?)</font></span>%', $html, $match))
+                    $this->stanje = $match[1];
+                else {
+                    $errorMsg = 'Greska prilikom ocitavanja stanja';
+                    return false;
+                }
+                if(preg_match('%<span id="rightframe_RazinaPravaLabel"><b><font face="Verdana" size="2">([^ ]+?) kompletna obroka dnevno</font></b></span>%', $html, $match))
+                    $this->razinaPrava = $match[1];
+                else {
+                    $errorMsg = 'Greska prilikom ocitavanja prava';
+                    return false;
+                }
+                if(preg_match('%<img id="rightframe_Image1" src="TempPic\\\\([0-9]+?).jpg" height="150" width="150" />%', $html, $match))
+                    $this->slikaURL = "/TempPic\\{$match[1]}.jpg";
+                else {
+                    $errorMsg = 'Greska prilikom ocitavanja slike';
+                    return false;
+                }
+            } else {
+                $errorMsg = 'Greska x02';
+                return false;
+            }
+        } else {
+            $errorMsg = 'Greska x01';
+            return false;
+        }
+
+    }
+
+    public function pregledRacuna(&$errorMsg = '') {
+        if(!empty($this->racuni))
+            return $this->racuni;
+                    
+        $html = $this->aspParser->getData(ASPParser::METHOD_GET, 'http://www.cap.srce.hr/Dnevniknovo.aspx');
 		$html=str_replace("\r\n","",$html);
 		$pat='/__doPostBack[^>]+>([^<]+)<.a>[^<]+<\/td><td>([^<]+)<\/td><td>([^<]+)<\/td>[^>]*>([^<]+)/';
+
+        $this->racuni = array();
 		if(preg_match_all($pat,$html,$match)) {
 			for($i=0;$i<count($match[0]);$i++) {
-				$ret["racuni"][$i]["vrijeme"]=$match[1][$i];
-				$ret["racuni"][$i]["restoran"]=$match[2][$i];
-				$ret["racuni"][$i]["linija"]=$match[3][$i];
-				$ret["racuni"][$i]["iznos"]=$match[4][$i];
-				
-				$date = new DateTime($ret["racuni"][$i]["vrijeme"]);
-				if(date_format($date,"d")==date("d"))
-					$spent += (float) str_replace(",", ".",$ret["racuni"][$i]["iznos"]);
+				$this->racuni[] = array(
+                    'vrijeme' => $match[1][$i],
+                    'restoran' => $match[2][$i],
+                    'linija' => $match[3][$i],
+                    'iznos' => $match[4][$i]);
+
+                $date = new DateTime($this->racuni[$i]["vrijeme"]);
+
+                if(date_format($date,"d")==date("d"))
+					$this->danasPotroseno += (float) str_replace(",", ".",$this->racuni[$i]["iznos"]);
 			}
 		}
-		$ret["ostalo"] = $prava[$ret["razina"]]-$spent;
-		
-		$ret["potroseno_sada"] = $spent_total;
-		$ret["potroseno_kraj_mj"] = round($spent_total + ($spent_total / ((int) date("d"))) * ((int)date("t")-(int)date("d")), 2);
-		
-		$asp_data=$http->parse_asp($html);
-		// save 3 file
-		file_put_contents("cache/$jmbag.txt",json_encode($ret));
-		file_put_contents("cache/$jmbag.aspdata.txt",$http->parse_asp($html));
-		file_put_contents("cache/$jmbag.cookie.txt", implode(";", $http->GetCookie()));
-	}else{
-		$ret=json_decode($cache, true);
-		$http->SetCookie(explode(";", $cookie));
-	}
-	
-	if($racun!=""){
-		if($racun<9)
-			$target="0".($racun+1);
+
+        if(preg_match('%<span id="rightframe_Label2">([0-9,]+)</span>%', $html, $match)) {
+            $this->ukupnoPotroseno = $match[1];
+        } else {
+            $errorMsg = 'Greska prilikom ocitavanja ukupne potrosnje';
+            return false;
+        }
+
+        return $this->racuni;
+    }
+
+    public function racunInfo($racunBroj) {
+		if($racunBroj < 9)
+			$racunBroj = '0'.($racunBroj + 1);
 		else
-			$target=$racun+1;
-		$html=$http->PostHTML("www.cap.srce.hr","/Dnevniknovo.aspx","__EVENTTARGET="."ctl00%24rightframe%24DataGrid1%24ctl".$target."%24lb".$asp_data);
+			$racunBroj = $racunBroj + 1;
+
+        $html = $this->aspParser->getData(ASPParser::METHOD_POST, "http://www.cap.srce.hr/Dnevniknovo.aspx", array("__EVENTTARGET" => 'ctl00$rightframe$DataGrid1$ctl'.$racunBroj.'$lb'));
+
 		$ret=array();
-		if(ereg('<span id="ctl00_rightframe_Label5">([^<]+)</span>',$html,$match))
-			$ret["opis"]=$match[1];
-		if(ereg('<span id="ctl00_rightframe_Label1">([^<]+)</span>',$html,$match))
-			$ret["vrijeme"]=$match[1];
-		if(ereg('<span id="ctl00_rightframe_Label2">([^<]+)</span>',$html,$match))
-			$ret["iznos"]=$match[1];
-		if(ereg('<span id="ctl00_rightframe_Label4">([^<]+)</span>',$html,$match))
-			$ret["participacija"]=$match[1];
-		$ret["artikli"]=array();
-		$pat='/<td>([^<]+)<\/td><td align="center">([^<]+)<\/td><td align="right">[^<]+<\/td><td align="right">([^<]+)<\/td><td align="right">([^<]+)<\/td>/';
-		if(preg_match_all($pat,$html,$match)) {
-			for($i=0;$i<count($match[0]);$i++) {
-				$ret["artikli"][$i]["naziv"]=$match[1][$i];
-				$ret["artikli"][$i]["komada"]=$match[2][$i];
-				$ret["artikli"][$i]["ukupno"]=$match[3][$i];
-				$ret["artikli"][$i]["subvencija"]=$match[4][$i];
+		if(preg_match('%<span id="rightframe_Label5"><font face="Verdana" size="2">([^<]+)</font></span>%', $html, $match))
+			$ret['opis']=$match[1];
+		if(preg_match('%<span id="rightframe_Label1"><font face="Verdana" size="2">([^<]+)</font></span>%', $html, $match))
+			$ret['vrijeme']=$match[1];
+		if(preg_match('%<span id="rightframe_Label2" class="smalltextcell">([^<]+)</span>%', $html, $match))
+			$ret['iznos']=$match[1];
+        if(preg_match('%<span id="rightframe_Label4" class="smalltextcell">([^<]+)</span>%', $html, $match))
+			$ret['participacija']=$match[1];
+
+		$ret['artikli']=array();
+
+		$pat='%<td>([^<]+)</td><td align="center">([^<]+)</td><td align="right">[^<]+</td><td align="right">([^<]+)</td><td align="right">([^<]+)</td>%';
+		if(preg_match_all($pat, $html, $match)) {
+			for($i = 0; $i < count($match[0]); $i++) {
+				$ret['artikli'][$i]['naziv'] = $match[1][$i];
+				$ret['artikli'][$i]['komada'] = $match[2][$i];
+				$ret['artikli'][$i]['ukupno'] = $match[3][$i];
+				$ret['artikli'][$i]['subvencija'] = $match[4][$i];
 			}
 		}
-	}
-	echo json_encode($ret);
-?>
+        return $ret;
+    }
+
+    public function toJson() {
+        return json_encode(array(
+            'korisnik' => $this->korisnik,
+            'stanje' => $this->stanje,
+            'razina' => $this->razinaPrava,
+            'slika' => $this->slikaURL,
+            'racuni' => $this->racuni,
+            'ostalo' => $this->prava[$this->razinaPrava]-$this->danasPotroseno,
+            'potroseno_sada' => $this->ukupnoPotroseno,
+            'potroseno_kraj_mj' => round($this->ukupnoPotroseno + ($this->ukupnoPotroseno / ((int) date("d"))) * ((int)date("t")-(int)date("d")), 2)
+        ));
+    }
+}
+
+$xica = new Xica();
+if($xica->login($_REQUEST['username'], $_REQUEST['password'], $error) == false)
+    die('FAIL:' . $error);
+
+$xica->pregledRacuna();
+
+if(isset($_REQUEST['racun']))
+    echo json_encode($xica->racunInfo($_GET['racun']));
+else
+    echo $xica->toJson();
